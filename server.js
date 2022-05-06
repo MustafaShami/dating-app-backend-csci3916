4bae5a95a45d23478d305c750590358f53a1a1e6
MustafaShami <mustafa.shami@ucdenver.edu>
2022-05-06 04:31:26 -0600

    connected to database

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ import mongoose from 'mongoose'
 //App Configuration and tell what port to listen on
 const app = express()
 const port = process.env.PORT || 8080
+const connection_url = 'mongodb+srv://MustafaShami:finalapi@cluster0.2vuaf.mongodb.net/dating-app-MERN?retryWrites=true&w=majority'
+
+//Database Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
 
 
 //API endpoints
